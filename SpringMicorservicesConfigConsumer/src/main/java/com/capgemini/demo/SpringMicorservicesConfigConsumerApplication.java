34fc7952d53c029c0d19cb7505e1fde443a45dc6
Rupesh Patil <rupeshrp12@gmail.com>
2020-05-04 17:04:31 +0530

    Consumer and server application pushed

---
+++ SpringMicorservicesConfigConsumer/src/main/java/com/capgemini/demo/SpringMicorservicesConfigConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.capgemini.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMicorservicesConfigConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMicorservicesConfigConsumerApplication.class, args);
+	}
+
+}
