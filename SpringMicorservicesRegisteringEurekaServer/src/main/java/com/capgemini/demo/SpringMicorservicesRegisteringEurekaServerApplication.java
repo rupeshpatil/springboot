f00392a7cb1345bedcecab8c2a7111e814bd4b84
Rupesh Patil <rupeshrp12@gmail.com>
2020-05-05 17:39:15 +0530

    Register services with eureka

---
+++ SpringMicorservicesRegisteringEurekaServer/src/main/java/com/capgemini/demo/SpringMicorservicesRegisteringEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.capgemini.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMicorservicesRegisteringEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMicorservicesRegisteringEurekaServerApplication.class, args);
+	}
+
+}
